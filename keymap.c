1658bf896186818ca47e0f5280e76f4ccfd21285
Ivan Svoboda <ivanisekeev0@gmail.com>
2023-03-06 19:35:33 +0600

    refactoring

--- keymap.c
+++ keymap.c
@@ -1,21 +1,15 @@
 #include QMK_KEYBOARD_H
 
 #define CUSTOM_SAFE_RANGE SAFE_RANGE
-#include "lang_shift/include.h"
 
 #include <stdio.h>
 #include <stdint.h>
 
-
 #define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_CUR 5
-#define L_SYM 6
-#define L_SPEC 7
-#define L_ADJUST 8
-#define L_GAME 9
-#define L_GAME_CUR 10
+#define L_CUR 1
+#define L_SYM 2
+#define L_GAME 3
+#define L_GAME_CUR 4
 
 // English layer
 #define H_O LALT_T(KC_O)
@@ -28,18 +22,15 @@
 #define H_N RGUI_T(KC_N)
 #define H_S LALT_T(KC_S)
 
-
 // Symbol layer
-#define H_SLSH TD(ALT_SL)
-#define H_MINS TD(GUI_MN)
+#define H_SLSH LALT_T(KC_SLSH)
+#define H_MINS LGUI_T(KC_MINS)
 #define H_LCBR TD(CTL_LB)
 #define H_RCBR TD(SFT_RB)
 
 #define H_LPRN TD(SFT_LP)
 #define H_RPRN TD(CTL_RP)
-#define H_QUOT TD(GUI_QT)
-#define H_COLN LALT_T(KC_NO)
-
+#define H_QUOT RGUI_T(KC_QUOT)
 
 // Cursor layer
 #define H_HOME LALT_T(KC_HOME)
@@ -51,7 +42,6 @@
 #define H_5 RCTL_T(KC_5)
 #define H_6 RGUI_T(KC_6)
 #define H_NO LALT_T(KC_NO)
-
 
 // Layer keys
 #define CUR_SPC LT(L_CUR, KC_SPC)
@@ -66,15 +56,11 @@ enum custom_keycodes {
 
 // Tap Dance keycodes
 enum td_keycodes {
-    ALT_SL,
-    GUI_MN,
     CTL_LB,
     SFT_RB,
 
     SFT_LP,
     CTL_RP,
-    GUI_QT,
-    ALT_CL,
 };
 
 // Define a type containing as many tap-dance states as you need
@@ -109,57 +95,57 @@ void guiqt_reset(qk_tap_dance_state_t *state, void *user_data);
 #define MO_GAME_CUR MO(L_GAME_CUR)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_split_3x6_3(
+  [L_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       TG_GAME,    KC_Q,    KC_F,    KC_U,    KC_Y,    KC_Z,                         KC_X,    KC_K,    KC_C,    KC_W,    KC_B, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BSLS,     H_O,     H_H,     H_E,     H_A,    KC_I,                         KC_D,     H_R,     H_T,     H_N,     H_S, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, AG_COMM,    KC_M,  AG_DOT,    KC_J, AG_SCLN,                         KC_G,    KC_L,    KC_P,    KC_V, KC_LBRC, XXXXXXX,
+      XXXXXXX, KC_COMM,    KC_M,  KC_DOT,    KC_J, KC_SCLN,                         KC_G,    KC_L,    KC_P,    KC_V, KC_LBRC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, KC_TAB, SYM_ENT,   CUR_SPC, KC_PSCR,  KC_CAPS
                                       //`--------------------------'  `--------------------------'
   ),
   [L_CUR] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, KC_PGUP, KC_BSPC,   KC_UP,  KC_DEL, KC_PGDN,                      XXXXXXX,    AG_1,    AG_2,    AG_3, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_PGUP, KC_BSPC,   KC_UP,  KC_DEL, KC_PGDN,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,  H_HOME,  H_LEFT,  H_DOWN, H_RIGHT,  KC_END,                         AG_0,     H_4,     H_5,     H_6,    H_NO, XXXXXXX,
+      XXXXXXX,  H_HOME,  H_LEFT,  H_DOWN, H_RIGHT,  KC_END,                         KC_0,     H_4,     H_5,     H_6, KC_LALT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, CTL_PGU, CTL_PGD, XXXXXXX,                      XXXXXXX,    AG_7,    AG_8,    AG_9, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, CTL_PGU, CTL_PGD, XXXXXXX,                      XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_TRNS, KC_ESC, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS
+                                           _______, KC_ESC, _______,  _______, _______,  _______
                                       //`--------------------------'  `--------------------------'
   ),
   [L_SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, AG_DQUO, AG_UNDS, EN_LBRC, EN_RBRC, EN_CIRC,                      AG_EXCL,   EN_LT,   EN_GT,  AG_EQL, EN_AMPR, XXXXXXX,
+      XXXXXXX, KC_DQUO, KC_UNDS, KC_LBRC, KC_RBRC, KC_CIRC,                      KC_EXLM,   KC_LT,   KC_GT,  KC_EQL, KC_AMPR, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, H_SLSH,  H_MINS,  H_LCBR,  H_RCBR,  AG_ASTR,                      AG_QUES,   H_LPRN, H_RPRN,  H_QUOT,  H_COLN, KC_TRNS,
+      XXXXXXX, H_SLSH,  H_MINS,  H_LCBR,  H_RCBR,  KC_ASTR,                      KC_QUES,   H_LPRN, H_RPRN,  H_QUOT, KC_LALT, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, EN_HASH,  EN_DLR, EN_PIPE, EN_TILD,  EN_GRV,                      AG_PLUS, AG_PERC, AG_BSLS,   EN_AT, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_HASH, KC_DLR, KC_PIPE, KC_TILD,  KC_GRV,                       KC_PLUS,  KC_PERC, KC_BSLS,   KC_AT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
+                                          _______, _______, _______,  _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
   [L_GAME] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      TG_GAME,    KC_Q,    KC_F,    KC_U,    KC_Y,    KC_Z,                         KC_X,    KC_K,    KC_C,    KC_W,    KC_B, XXXXXXX,
+      TG_GAME,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,       KC_O, KC_P, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_BSLS,    KC_O,    KC_H,    KC_E,    KC_A,    KC_I,                           KC_D,     H_R,    H_T,    H_N,     H_S, KC_RBRC,
+      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,        KC_K,    KC_L, KC_SCLN, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, AG_COMM,    KC_M,  AG_DOT,    KC_J, AG_SCLN,                         KC_G,    KC_L,    KC_P,    KC_V, KC_LBRC, XXXXXXX,
+      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM,    KC_DOT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, KC_TAB, SYM_ENT,   MO_GAME_CUR, KC_PSCR, KC_CAPS
+                                           KC_SPC, KC_TAB, SYM_ENT,   MO_GAME_CUR, KC_PSCR, KC_CAPS
                                       //`--------------------------'  `--------------------------'
   ),
   [L_GAME_CUR] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, KC_PGUP, KC_BSPC, KC_UP,  KC_DEL,  KC_PGDN,                      XXXXXXX,    AG_1,    AG_2,    AG_3, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_PGUP, KC_BSPC, KC_UP,  KC_DEL,  KC_PGDN,                      XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, H_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,                         AG_0,     H_4,     H_5,     H_6,    H_NO, XXXXXXX,
+      XXXXXXX, H_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,                         KC_0,     H_4,     H_5,     H_6,    H_NO, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, CTL_PGU, CTL_PGD, XXXXXXX,                      XXXXXXX,    AG_7,    AG_8,    AG_9, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, CTL_PGU, CTL_PGD, XXXXXXX,                      XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, KC_ESC, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -248,40 +234,6 @@ void ctlrp_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void guiqt_finished(qk_tap_dance_state_t *state, void *user_data) {
-    td_state = cur_dance(state);
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            register_code16(KC_QUOT);
-            break;
-        case TD_SINGLE_HOLD:
-            register_mods(MOD_BIT(KC_RGUI)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
-            tap_code16(KC_QUOT);
-            register_code16(KC_QUOT);
-            break;
-        default:
-            break;
-    }
-}
-
-void guiqt_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            unregister_code16(KC_QUOT);
-            break;
-        case TD_SINGLE_HOLD:
-            unregister_mods(MOD_BIT(KC_RGUI)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP:
-            unregister_code16(KC_QUOT);
-            break;
-        default:
-            break;
-    }
-}
-
 void sftrb_finished(qk_tap_dance_state_t *state, void *user_data) {
     td_state = cur_dance(state);
     switch (td_state) {
@@ -350,89 +302,20 @@ void ctllb_reset(qk_tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void guimn_finished(qk_tap_dance_state_t *state, void *user_data) {
-    td_state = cur_dance(state);
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            register_code16(KC_MINS);
-            break;
-        case TD_SINGLE_HOLD:
-            register_mods(MOD_BIT(KC_LGUI)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
-            tap_code16(KC_MINS);
-            register_code16(KC_MINS);
-            break;
-        default:
-            break;
-    }
-}
-
-void guimn_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            unregister_code16(KC_MINS);
-            break;
-        case TD_SINGLE_HOLD:
-            unregister_mods(MOD_BIT(KC_LGUI)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP:
-            unregister_code16(KC_MINS);
-            break;
-        default:
-            break;
-    }
-}
-
-void altsl_finished(qk_tap_dance_state_t *state, void *user_data) {
-    td_state = cur_dance(state);
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            register_code16(KC_SLSH);
-            break;
-        case TD_SINGLE_HOLD:
-            register_mods(MOD_BIT(KC_LALT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
-            tap_code16(KC_SLSH);
-            register_code16(KC_SLSH);
-            break;
-        default:
-            break;
-    }
-}
-
-void altsl_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            unregister_code16(KC_SLSH);
-            break;
-        case TD_SINGLE_HOLD:
-            unregister_mods(MOD_BIT(KC_LALT)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
-            break;
-        case TD_DOUBLE_SINGLE_TAP:
-            unregister_code16(KC_SLSH);
-            break;
-        default:
-            break;
-    }
-}
-
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [ALT_SL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, altsl_finished, altsl_reset),
-    [GUI_MN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, guimn_finished, guimn_reset),
-    [CTL_LB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctllb_finished, ctllb_reset), 
-    [SFT_RB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sftrb_finished, sftrb_reset), 
+    [CTL_LB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctllb_finished, ctllb_reset),
+    [SFT_RB] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sftrb_finished, sftrb_reset),
 
-    [SFT_LP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sftlp_finished, sftlp_reset), 
-    [CTL_RP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctlrp_finished, ctlrp_reset), 
-    [GUI_QT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, guiqt_finished, guiqt_reset), 
+    [SFT_LP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sftlp_finished, sftlp_reset),
+    [CTL_RP] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ctlrp_finished, ctlrp_reset),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!lang_shift_process_record(keycode, record)) return false;
-
     switch (keycode) {
+        case KEYCODES_START:
+            if (record->event.pressed) {
+            }
+            return false; // Skip all further processing of this key
         case CTL_PGU:
             if (record->event.pressed) {
                 register_code(KC_LCTL);
@@ -451,12 +334,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
     }
     return true;
-}
-
-void user_timer(void) {
-    lang_shift_user_timer();
-}
-
-void matrix_scan_user(void) {
-    user_timer();
 }
